1a530536318f77cb82aaba311763d496e06a8299
Qian Wei <mjsrbook@gmail.com>
2012-05-27 12:36:21 +0800

    add gemfile gem 'rake','0.8.7'> 	modified:   Gemfile 	modified:   Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(:version => 20110204230528) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "quantity",   :default => 1
-    t.decimal  "price"
+    t.integer  "quantity",                                 :default => 1
+    t.decimal  "price",      :precision => 8, :scale => 2
     t.integer  "order_id"
   end
 
